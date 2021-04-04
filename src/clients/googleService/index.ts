fe30a8edfe4d8fd943f2e3f054bcc4b71630f116
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-04-04 20:53:51 -0300

    fix: rename arg getAdapter

--- src/clients/googleService/index.ts
+++ src/clients/googleService/index.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 import { getAdapter } from './../../common/core/https'
 
 class GoogleService {
-  async geocode (number: string): Promise <any> {
+  async geocode (address: string): Promise <any> {
     return await getAdapter({
-      url: process.env.GOOGLE_URL_GEOCODE + `json?address=${number}&key=` + process.env.GOOGLE_KEY
+      url: process.env.GOOGLE_URL_GEOCODE + `json?address=${address}&key=` + process.env.GOOGLE_KEY
     })
   }
 }
