9b6a4bee0d142ad9af7e45235bdae2f2f8ae806a
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-04-13 21:19:48 -0300

    feat: add headers authorization

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -5,6 +5,17 @@ import CepAdapter from './cepAdapter/index'
 const routes = express.Router()
 const cep = new CepAdapter()
 
+routes.use(function (req, res, next) {
+  console.log()
+  if (req.headers.authorization === process.env.AUTHORIZATION) {
+    next()
+  }
+  res.status(401).json({
+    message: 'Unauthorized',
+    info: 'Contact the developer'
+  })
+})
+
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 routes.post('/cep', cep.post)
 export default routes
