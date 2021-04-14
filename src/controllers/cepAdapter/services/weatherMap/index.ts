04bbdaedd1dafd02509c220e47a18f503fae7cdd
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-04-13 21:48:58 -0300

    feat: adicionando uma validacao caso de erro

--- src/controllers/cepAdapter/services/weatherMap/index.ts
+++ src/controllers/cepAdapter/services/weatherMap/index.ts
@@ -2,5 +2,7 @@ import IWeather from 'src/clients/weather/IWeather'
 import WeatherMap from '../../../../clients/weather/index'
 
 export async function receiveWeatherMap (params: IWeather): Promise<any> {
-  return await WeatherMap.get(params)
+  const { data, status } = await WeatherMap.get(params)
+  if (status !== 200) throw new Error(`Erro - receiveWeatherMap - ${status}`)
+  return data
 }
