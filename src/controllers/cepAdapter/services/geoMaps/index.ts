2a1b76428a2e90b68a59cd9c5de99dcd8e87c040
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-04-13 21:48:37 -0300

    feat: adicionando um filtro para caso de erro

--- src/controllers/cepAdapter/services/geoMaps/index.ts
+++ src/controllers/cepAdapter/services/geoMaps/index.ts
@@ -2,7 +2,8 @@ import GoogleService from '../../../../clients/googleService/index'
 import { filterLocation } from './filterLocation'
 export async function receiveGeoMaps (address: string): Promise<any> {
   try {
-    const { data } = await GoogleService.geocode(encodeURI(address))
+    const { data, status } = await GoogleService.geocode(encodeURI(address))
+    if (status !== 200) throw new Error('Erro - receiveGeoMaps')
     return filterLocation(data)
   } catch (error) {
     throw Error('Erro no receiveGeoMaps')
