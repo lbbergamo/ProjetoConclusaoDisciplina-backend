bbe94708e4bf244a7d3673f9b8208a6f52f4184c
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-04-13 21:20:15 -0300

    fix: removendo comentario

--- src/controllers/cepAdapter/index.ts
+++ src/controllers/cepAdapter/index.ts
@@ -29,26 +29,4 @@ class CepAdapter {
   }
 }
 
-/**
- *
-{
-  "statusCode": "200",
-  "CEP": "11040-010",
-  "Endereco": {
-      "logradouro": "Rua Alexandre Fleming",
-      "bairro": "Aparecida",
-      "localidade": "Santos",
-      "uf": "SP",
-      "cep": "11040010"
-  },
-  "tempo": {
-    "status": "chovendo",
-    "temperatura": "23"
-  },
-  "googleMaps": {
-    "latitude": "-23.9736663",
-    "longitude": "-46.3105719"
-  }
-}
- */
 export default CepAdapter
