497c5106ee62d90706ac295f6b7a651f0b683f71
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-05-18 23:22:01 -0300

    fix: return path

--- src/clients/firebase/functions/image.ts
+++ src/clients/firebase/functions/image.ts
@@ -1,16 +1,19 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import myAdmin from './../credentials'
 import fs from 'fs'
 import path from 'path'
 
 export default async (imageName: string) => {
   const bucket = myAdmin.storage().bucket()
-  bucket.file(imageName).download().then(async (data: Array<WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>>) => {
-    const buffer = Buffer.from(data[0])
-    fs.writeFile(path.resolve(__dirname, '..', 'uploads', 'image', imageName), buffer, 'binary', (err) => {
-      console.log(err)
+  return await bucket.file(imageName)
+    .download()
+    .then(async (data: Array<WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>>) => {
+      const buffer = Buffer.from(data[0])
+      fs.writeFile(path.resolve('.', 'uploads', 'image', imageName), buffer, 'binary', (err) => {
+        console.log(err)
+      })
     })
-  }).catch((err: any) =>
-    console.log(`Não foi possivel baixar a imagem: ${err}`))
-
-  return 'uploads/image/' + imageName
+    .then(() => `uploads/image/${imageName}`)
+    .catch(error => { throw new Error(error.message) })
 }
