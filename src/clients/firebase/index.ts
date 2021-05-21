98e7add7b2876e12f4facadae8740fe5499d6da3
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-05-20 22:31:11 -0300

    feat: handling the argument

--- src/clients/firebase/index.ts
+++ src/clients/firebase/index.ts
@@ -3,7 +3,7 @@ import getImage from './functions/image'
 
 export default async (id: string): Promise<object> => {
   const data = await getInfo(id)
-  const image = await getImage(data.imageUrl)
+  const image = await getImage(data.imageUrl.split('/')[1])
   return {
     ...data,
     url: image
