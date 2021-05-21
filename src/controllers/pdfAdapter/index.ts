5b7f52dbd2b446115ff8c3517588a113ed08fae0
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-05-21 00:06:41 -0300

    feat: index fuction

--- src/controllers/pdfAdapter/index.ts
+++ src/controllers/pdfAdapter/index.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express'
 
 import firebase from '../../clients/firebase/index'
+
+import pdf from './functions/generate'
 class PdfAdapter {
   async get (req: Request, res: Response): Promise<Response> {
     try {
-      const data = await firebase('1869177530')
-      return res.json({ message: 'Success request', body: data })
+      const id: string = req.query.chave
+      const bancoDeDados = await firebase(id)
+      const dados = await pdf(id.toString(), bancoDeDados)
+      return res.json({ message: 'Success request', url: dados })
     } catch (error) {
-      // console.log(error)
       return res.status(400).json({ message: error.message, erro: error })
     }
   }
