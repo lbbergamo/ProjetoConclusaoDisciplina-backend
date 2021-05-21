5b779b4b4fd38f7e3c3f2fbed4daebdc086666b7
Leandro Bergamo <leandro.bergamo@hotmail.com>
2021-05-20 23:57:16 -0300

    fix: fs to promises

--- src/clients/firebase/functions/image.ts
+++ src/clients/firebase/functions/image.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import myAdmin from './../credentials'
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import path from 'path'
 
 export default async (imageName: string) => {
@@ -10,8 +10,7 @@ export default async (imageName: string) => {
   return await bucket.file(imageName)
     .download()
     .then(async (data: Array<WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>>) => {
-      const buffer = Buffer.from(data[0])
-      fs.writeFile(path.resolve('.', 'public', 'uploads', 'image', imageName), buffer, 'binary', (err, data) => { })
+      await fs.writeFile(path.resolve('.', 'public', 'uploads', 'image', imageName), Buffer.from(data[0]), 'binary')
     })
     .then(() => `uploads/image/${imageName}`)
     .catch(error => { throw { message: error.message, status: "Couldn't recover image" } })
